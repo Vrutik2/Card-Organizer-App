3bb64968610e2d604a801a28883af4c72d22abd0
Vrutik Patel <vrutikpatel2022@gmail.com>
2024-10-17 17:20:22 -0400

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,96 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Card Organizer App',
+      title: 'SQFlite Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
     );
+  }
+}
+
+class FolderScreen extends StatelessWidget {
+  const FolderScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('sqflite'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: _insert,
+              child: const Text('insert'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _query,
+              child: const Text('query'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _update,
+              child: const Text('update'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _delete,
+              child: const Text('delete'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyHomePage()),
+                );
+              },
+              child: const Text('Cards Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  void _insert() async {
+
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnSuit: 'Spades',
+      DatabaseHelper.columnNum: 11
+    };
+    final id = await dbHelper.insert(row);
+    debugPrint('inserted row id: $id');
+  }
+
+  void _query() async {
+    final allRows = await dbHelper.queryAllRows();
+    debugPrint('query all rows:');
+    for (final row in allRows) {
+      debugPrint(row.toString());
+    }
+  }
+
+  void _update() async {
+
+    Map<String, dynamic> row = {
+      DatabaseHelper.folderId: 1,
+      DatabaseHelper.columnNum: 3,
+      DatabaseHelper.columnSuit: 'Clubs',
+    };
+    final rowsAffected = await dbHelper.update(row);
+    debugPrint('updated $rowsAffected row(s)');
+  }
+
+  void _delete() async {
+    final id = await dbHelper.queryRowCount();
+    final rowsDeleted = await dbHelper.delete(id);
+    debugPrint('deleted $rowsDeleted row(s): row $id');
   }
 }
 
@@ -38,23 +122,33 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
-              onPressed: _insertFolder,
-              child: const Text('Insert Folder'),
+              onPressed: _insert,
+              child: const Text('insert'),
             ),
             const SizedBox(height: 10),
             ElevatedButton(
-              onPressed: _queryFolders,
-              child: const Text('Query Folder'),
+              onPressed: _query,
+              child: const Text('query'),
             ),
             const SizedBox(height: 10),
             ElevatedButton(
-              onPressed: _updateFolder,
-              child: const Text('Update Folder'),
+              onPressed: _update,
+              child: const Text('update'),
             ),
             const SizedBox(height: 10),
             ElevatedButton(
-              onPressed: _deleteFolder,
-              child: const Text('Delete Folder'),
+              onPressed: _delete,
+              child: const Text('delete'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FolderScreen()),
+                );
+              },
+              child: const Text('Folder Page'),
             ),
           ],
         ),
@@ -63,39 +157,38 @@ class MyHomePage extends StatelessWidget {
   }
 
 
- void _insertFolder() async {
+  void _insert() async {
+
     Map<String, dynamic> row = {
-      DatabaseHelper.folderName: 'Hearts',
-      DatabaseHelper.columnSuit: 'Hearts'
+      DatabaseHelper.columnSuit: 'Spades',
+      DatabaseHelper.columnNum: 11
     };
     final id = await dbHelper.insert(row);
-    debugPrint('Inserted folder with id: $id');
+    debugPrint('inserted row id: $id');
   }
 
-  
-  void _queryFolders() async {
+  void _query() async {
     final allRows = await dbHelper.queryAllRows();
-    debugPrint('Query all folders:');
+    debugPrint('query all rows:');
     for (final row in allRows) {
       debugPrint(row.toString());
     }
   }
 
+  void _update() async {
 
-  void _updateFolder() async {
     Map<String, dynamic> row = {
-      DatabaseHelper.folderId: 1, 
-      DatabaseHelper.folderName: 'Red Cards', 
-      DatabaseHelper.columnSuit: 'Hearts',
+      DatabaseHelper.folderId: 1,
+      DatabaseHelper.columnNum: 3,
+      DatabaseHelper.columnSuit: 'Clubs',
     };
     final rowsAffected = await dbHelper.update(row);
-    debugPrint('Updated $rowsAffected folder(s)');
+    debugPrint('updated $rowsAffected row(s)');
   }
 
-
-  void _deleteFolder() async {
-    final id = 1; 
+  void _delete() async {
+    final id = await dbHelper.queryRowCount();
     final rowsDeleted = await dbHelper.delete(id);
-    debugPrint('Deleted $rowsDeleted folder(s): folder $id');
+    debugPrint('deleted $rowsDeleted row(s): row $id');
   }
 }
